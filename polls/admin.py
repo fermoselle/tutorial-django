38e9154b8fa41ffd0a50c640caf9c67bf9237566
David Fermoselle <fermoselledavid.web@gmail.com>
2020-09-17 14:29:55 +0200

    Feat: Añade choices a question

--- polls/admin.py
+++ polls/admin.py
@@ -14,11 +14,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     list_display = ("question_text", "pub_date", "was_published_recently")
+    inlines = [ChoiceInline]
     list_filter = ["pub_date"]
     search_fields = ["question_text"]
-
-    inlines = [ChoiceInline]
-
 
 
 admin.site.register(Question, QuestionAdmin)
